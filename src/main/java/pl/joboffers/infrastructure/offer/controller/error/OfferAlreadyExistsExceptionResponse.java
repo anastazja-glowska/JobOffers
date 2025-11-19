b23e94a8c80f26e1a821098219e66881d0845c07
Anastazja Glowska <anastazjaglowska12345@gmail.com>
2025-11-19 15:10:11 +0100

    Implemeting small changes in integration test

--- src/main/java/pl/joboffers/infrastructure/offer/controller/error/OfferAlreadyExistsExceptionResponse.java
+++
@@ -1,6 +0,0 @@
-package pl.joboffers.infrastructure.offer.controller.error;
-
-import org.springframework.http.HttpStatus;
-
-public record OfferAlreadyExistsExceptionResponse(String message, HttpStatus status) {
-}
