ddab4373e4ebb9d8f752ab8f4e5bef8d5f16162d
Anastazja Glowska <anastazjaglowska12345@gmail.com>
2025-11-22 20:26:09 +0100

    Implementing test for failed validation register

--- src/integration/java/pl/joboffers/apivalidationerror/ApiValidationFailedIntegrationTest.java
+++ src/integration/java/pl/joboffers/apivalidationerror/ApiValidationFailedIntegrationTest.java
@@ -123,6 +123,39 @@ public class ApiValidationFailedIntegrationTest extends BaseIntegrationTest {
         );
     }
 
+    @Test
+    @DisplayName("Should return status bad request 400 and validation message when user provide empty password and invalid email")
+    void should_return_status_bad_request_400_and_validation_message_when_user_provide_empty_password_and_invalid_email() throws Exception {
+
+        //given && when
+
+        ResultActions performedInvalidCredentials = mockMvc.perform(post("/register")
+                .content("""
+                        {
+                        "username" : "email",
+                        "password" : ""
+                        }
+                        """.trim()
+                ).contentType(MediaType.APPLICATION_JSON));
+
+        MvcResult result = performedInvalidCredentials.andExpect(status().isBadRequest()).andReturn();
+        String badRequestResponseJson = result.getResponse().getContentAsString();
+        ApiValidationErrorsDto errors = objectMapper.readValue(badRequestResponseJson, ApiValidationErrorsDto.class);
+
+        //then
+
+        assertAll(
+                () -> assertThat(errors.messages()).containsExactlyInAnyOrder(
+                        "password must have min 6 length size",
+                        "email must have correct format", "email must not be empty"),
+                () -> assertThat(errors.status()).isEqualTo(HttpStatus.BAD_REQUEST),
+                () -> assertThat(errors).isNotNull()
+
+        );
+
+
+    }
+
 
 
 
